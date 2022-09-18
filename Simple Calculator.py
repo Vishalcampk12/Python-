3ebc41a066f4f708537787711bb7e3cd9e6c1ace
Vishalcampk12 <113816354+Vishalcampk12@users.noreply.github.com>
2022-09-18 10:32:53 +0530

    Add files via upload

---
+++ Simple Calculator.py
@@ -0,0 +1,10 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+sum = a + b
+difference = a - b
+product = a * b
+quotient = a / b
+print("Sum: ", str(sum))
+print("Difference: ", str(difference))
+print("Product: ", str(product))
+print("Quotient: ", str(quotient))
